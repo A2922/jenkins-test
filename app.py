033bb87eb011af4177f3c9d010c1e460ea014baf
ANKIT SHARMA <83114338+A2922@users.noreply.github.com>
2024-06-17 18:44:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import sys
+
 # Predefined credentials
 USERNAME = "admin"
 PASSWORD = "password"
 
 def login():
+    # Use predefined credentials directly
+    entered_username = "admin"
+    entered_password = "password"
+    
     print("Welcome to the Login System")
-    entered_username = input("Enter your username: admin ")
-    entered_password = input("Enter your password: password ")
+    print(f"Username: {entered_username}")
+    print(f"Password: {entered_password}")
     
     if entered_username == USERNAME and entered_password == PASSWORD:
         print("Login Success! Welcome!")
