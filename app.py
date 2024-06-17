757edb5a9ea5f24584245c26f7a1e63996db9004
ANKIT SHARMA <83114338+A2922@users.noreply.github.com>
2024-06-17 18:19:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ PASSWORD = "password"
 
 def login():
     print("Welcome to the Login System")
-    entered_username = input("Enter your username: ")
-    entered_password = input("Enter your password: ")
+    entered_username = input("Enter your username: admin ")
+    entered_password = input("Enter your password: password ")
     
     if entered_username == USERNAME and entered_password == PASSWORD:
         print("Login Success! Welcome!")
